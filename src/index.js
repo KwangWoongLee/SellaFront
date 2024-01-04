2383ef1df486efbb3e9ee86744c06df5173ed5e9
KwangWoongLee <lgu4821@naver.com>
2024-01-04 17:16:55 +0900

    검색엔진

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
+import { Helmet } from 'react-helmet';
 
 ReactDOM.render(
   <React.StrictMode id="top">
+    <Helmet>
+      <meta name="naver-site-verification" content="7a1691caacf9a2af8147d63dfb9ea97a4ac6b060" />
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
